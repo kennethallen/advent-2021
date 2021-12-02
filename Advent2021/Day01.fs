1a40c475ab5dfe0bee37f36ca16723efbfcc32ef
Kenneth Allen <kallen@liberty.com.au>
2021-12-02 18:12:21 +1100

    Refactor day 01

--- Advent2021/Day01.fs
+++ Advent2021/Day01.fs
@@ -1,14 +1,14 @@
 ﻿module Day01
 
-let part1 ls = ds |> Seq.map int |> part1Impl
 let part1Impl ds =
   ds
   |> Seq.pairwise
   |> Seq.filter (fun (d0, d1) -> d1 > d0)
   |> Seq.length
+let part1 (ls: seq<string>) = ls |> Seq.map int |> part1Impl
 
-let part2 ds =
-  ds
+let part2 (ls: seq<string>) =
+  ls
   |> Seq.map int
   |> Seq.windowed 3
   |> Seq.map Array.sum
