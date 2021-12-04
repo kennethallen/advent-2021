8ab7b0bfffaa3f394317d902bf18b52a39b2ebde
Kenneth Allen <kenneth@mostly.me>
2021-12-04 19:42:01 +1100

    Solve day 03

---
+++ Advent2021.Test/Day03.fs
@@ -0,0 +1,18 @@
+module Day03
+
+open Xunit
+open System.IO
+open Day03
+
+let in0 = ["00100"; "11110"; "10110"; "10111"; "10101"; "01111"; "00111"; "11100"; "10000"; "11001"; "00010"; "01010"]
+let in1 = File.ReadAllLines "Input/03.txt"
+
+[<Fact>]
+let part1_in0 () = Assert.Equal(198, part1 in0)
+[<Fact>]
+let part1_in1 () = Assert.Equal(3549854, part1 in1)
+
+[<Fact>]
+let part2_in0 () = Assert.Equal(230, part2 in0)
+[<Fact>]
+let part2_in1 () = Assert.Equal(3765399, part2 in1)
---
+++ Advent2021/Day03.fs
@@ -0,0 +1,33 @@
+﻿module Day03
+
+open System
+
+let parseBin s = Convert.ToInt32(s, 2)
+
+let bitCounts (ls: seq<string>) =
+  let f = Seq.map2 (fun count char -> count + ([|char|] |> String |> parseBin))
+  let init = Seq.head ls |> Seq.map (fun _ -> 0)
+  Seq.fold f init ls
+
+let majBits ls =
+  bitCounts ls
+  |> Seq.map (fun n -> 2*n >= Seq.length ls)
+
+let part1 ls =
+  let f =
+    Seq.map (fun b -> if b then '1' else '0')
+    >> Array.ofSeq
+    >> String
+    >> parseBin
+  let majs = majBits ls
+  (majs |> f) * (majs |> Seq.map not |> f)
+
+let part2 ls =
+  let rec f (rems: seq<string>) i maj =
+    match rems |> List.ofSeq with
+    | [only] -> parseBin only
+    | _      ->
+      let targetBit = majBits rems |> Seq.item i |> (=) maj
+      let pred (l: string) = (l.[i] = '1') = targetBit
+      f (rems |> Seq.filter pred) (i+1) maj
+  (f ls 0 true) * (f ls 1 false)
