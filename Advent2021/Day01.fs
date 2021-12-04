9401cb7e72518a80f29dd882ee0826e1bbdc4959
Kenneth Allen <kenneth@mostly.me>
2021-12-04 19:41:54 +1100

    Reformat day 01

--- Advent2021/Day01.fs
+++ Advent2021/Day01.fs
@@ -1,12 +1,15 @@
 ﻿module Day01
 
-let part1Impl = Seq.pairwise
-             >> Seq.filter (fun (d0, d1) -> d1 > d0)
-             >> Seq.length
-let part1: (seq<string> -> int) = Seq.map int<string>
-                               >> part1Impl
+let part1Impl =
+  Seq.pairwise
+  >> Seq.filter (fun (d0, d1) -> d1 > d0)
+  >> Seq.length
+let part1: (seq<string> -> int) =
+  Seq.map int<string>
+  >> part1Impl
 
-let part2: (seq<string> -> int) = Seq.map int
-                               >> Seq.windowed 3
-                               >> Seq.map Array.sum
-                               >> part1Impl
+let part2: (seq<string> -> int) =
+  Seq.map int
+  >> Seq.windowed 3
+  >> Seq.map Array.sum
+  >> part1Impl
