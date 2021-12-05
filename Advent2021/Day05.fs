63d6f0f241b59d17c574e2d93e43079c46e3b32d
Kenneth Allen <kenneth@mostly.me>
2021-12-06 00:45:53 +1100

    Optimize day 05

--- Advent2021/Day05.fs
+++ Advent2021/Day05.fs
@@ -8,43 +8,37 @@ let parseLine s =
   let [_; x0; y0; x1; y1] = (regex.Match s).Groups |> List.ofSeq
   ((int x0.Value, int y0.Value), (int x1.Value, int y1.Value))
 
-let onLineSimple ((x0: int, y0: int), (x1, y1)) (x, y) =
+let enumSeg diags ((x0: int, y0: int), (x1, y1)) =
+  let xMin = Math.Min(x0, x1)
+  let xMax = Math.Max(x0, x1)
+  let yMin = Math.Min(y0, y1)
+  let yMax = Math.Max(y0, y1)
+  let count = Math.Max(xMax - xMin, yMax - yMin)
+  let enumPoints f = seq { 0 .. count } |> Seq.map f
   if x0 = x1 then
-    x = x0 && y <= Math.Max(y0, y1) && y >= Math.Min(y0, y1)
+    enumPoints (fun i -> (x0, yMin + i))
   elif y0 = y1 then
-    y = y0 && x <= Math.Max(x0, x1) && x >= Math.Min(x0, x1)
+    enumPoints (fun i -> (xMin + i, y0))
+  elif diags then
+    if x1 - x0 = y1 - y0 then
+      enumPoints (fun i -> (xMin + i, yMin + i))
+    else
+      enumPoints (fun i -> (xMin + i, yMax - i))
   else
-    false
-let onLineFull ((x0: int, y0: int), (x1, y1)) (x, y) =
-  x <= Math.Max(x0, x1) && x >= Math.Min(x0, x1)
-  && y <= Math.Max(y0, y1) && y >= Math.Min(y0, y1)
-  && (x - x0) * (y1 - y0) = (y - y0) * (x1 - x0)
+    Seq.empty
 
-let calcBounds lines =
-  let (coords0, coords1) = lines |> List.unzip
-  let (xs0, ys0) = coords0 |> List.unzip
-  let (xs1, ys1) = coords1 |> List.unzip
-  let xs = List.append xs0 xs1
-  let ys = List.append ys0 ys1
-  (xs |> List.min, xs |> List.max, ys |> List.min, ys |> List.max)
-let allCoords (xMin, xMax, yMin, yMax) =
-  seq { xMin .. xMax }
-  |> Seq.collect (fun x ->
-    seq { yMin .. yMax }
-    |> Seq.map (fun y -> (x, y)))
+let countPoint map point =
+  map
+  |> Map.change point (fun n ->
+    Some (defaultArg n 0 |> (+) 1))
 
-let run lineCoordTest ls =
-  let lines = ls |> Seq.map parseLine |> List.ofSeq
-  let wins =
-    lines
-    |> calcBounds
-    |> allCoords
-    |> Seq.filter (fun coord ->
-      lines
-      |> Seq.filter (fun line -> lineCoordTest line coord)
-      |> Seq.length
-      |> (<=) 2)
-  wins |> Seq.length
+let run diags =
+  Seq.map parseLine
+  >> Seq.collect (enumSeg diags)
+  >> Seq.fold countPoint Map.empty
+  >> Map.values
+  >> Seq.filter ((<=) 2)
+  >> Seq.length
 
-let part1: (seq<string> -> int) = run onLineSimple
-let part2: (seq<string> -> int) = run onLineFull
+let part1: (seq<string> -> int) = run false
+let part2: (seq<string> -> int) = run true
