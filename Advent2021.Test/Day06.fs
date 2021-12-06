1d6bce5d16ab5a66f60b2d0d8cae0bde7510c774
Kenneth Allen <kenneth@mostly.me>
2021-12-06 23:01:28 +1100

    Solve day 06

---
+++ Advent2021.Test/Day06.fs
@@ -0,0 +1,18 @@
+module Day06
+
+open Xunit
+open System.IO
+open Day06
+
+let in0 = ["3,4,3,1,2"]
+let in1 = File.ReadAllLines "Input/06.txt"
+
+[<Fact>]
+let part1_in0 () = Assert.Equal(5934UL, part1 in0)
+[<Fact>]
+let part1_in1 () = Assert.Equal(362346UL, part1 in1)
+
+[<Fact>]
+let part2_in0 () = Assert.Equal(26984457539UL, part2 in0)
+[<Fact>]
+let part2_in1 () = Assert.Equal(1639643057051UL, part2 in1)
---
+++ Advent2021/Day06.fs
@@ -0,0 +1,27 @@
+﻿module Day06
+
+let mergeCountMap (m: Map<'a, uint64>) (k, (a: uint64)) =
+  Map.change k (fun n -> Some (defaultArg n 0UL |> (+) a)) m
+let countMapOfSeq = Seq.fold mergeCountMap Map.empty
+
+let sim days (ls: seq<string>) =
+  let init =
+    (Seq.head ls).Split ","
+    |> Seq.map int
+    |> Seq.map (fun n -> (n, 1UL))
+    |> countMapOfSeq
+  let step m =
+    m
+    |> Map.toSeq
+    |> Seq.collect (fun (f, n) ->
+      match f with
+      | 0 -> [6, n; 8, n]
+      | f -> [f-1, n])
+    |> countMapOfSeq
+  Seq.init days (fun _ -> step)
+  |> Seq.fold (|>) init
+  |> Map.values
+  |> Seq.sum
+
+let part1: (seq<string> -> uint64) = sim 80
+let part2: (seq<string> -> uint64) = sim 256
