5c8f05fd23148761e78873b4d8b1f1599ffa9421
Kenneth Allen <kenneth@mostly.me>
2021-12-07 19:17:01 +1100

    Solve day 07

---
+++ Advent2021.Test/Day07.fs
@@ -0,0 +1,18 @@
+module Day07
+
+open Xunit
+open System.IO
+open Day07
+
+let in0 = ["16,1,2,0,4,2,7,1,2,14"]
+let in1 = File.ReadAllLines "Input/07.txt"
+
+[<Fact>]
+let part1_in0 () = Assert.Equal(37, part1 in0)
+[<Fact>]
+let part1_in1 () = Assert.Equal(344735, part1 in1)
+
+[<Fact>]
+let part2_in0 () = Assert.Equal(168, part2 in0)
+[<Fact>]
+let part2_in1 () = Assert.Equal(96798233, part2 in1)
---
+++ Advent2021/Day07.fs
@@ -0,0 +1,13 @@
+﻿module Day07
+
+open System
+
+let calc costFn (ls: seq<string>) =
+  let cs = ls |> Seq.collect (fun l -> l.Split(",")) |> Seq.map int |> List.ofSeq
+  seq { List.min cs .. List.max cs }
+  |> Seq.map (fun x ->
+    cs |> Seq.sumBy (fun c -> Math.Abs(c - x) |> costFn))
+  |> Seq.min
+
+let part1: (seq<string> -> int) = calc id
+let part2: (seq<string> -> int) = calc (fun d -> d * (d+1) / 2)
