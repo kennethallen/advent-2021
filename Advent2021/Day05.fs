0f0b47335ef5eefceb33deaac1eca81981e8caec
Kenneth Allen <kenneth@mostly.me>
2021-12-09 23:47:39 +1100

    Optimize day 05

--- Advent2021/Day05.fs
+++ Advent2021/Day05.fs
@@ -3,6 +3,13 @@
 open System
 open System.Text.RegularExpressions
 
+let mi (a: int) (b: int) = Math.Min(a, b)
+let ma (a: int) (b: int) = Math.Max(a, b)
+
+type LineType = Horiz | Vert | ForwardDiag | BackDiag
+
+type LineSeg = (int*int) * (int*int) * LineType
+
 let regex = Regex "^(\d+),(\d+) -> (\d+),(\d+)$"
 let parseLine s =
   let [|x0; y0; x1; y1|] =
@@ -10,38 +17,69 @@ let parseLine s =
     |> Seq.tail
     |> Seq.map (fun g -> int g.Value)
     |> Array.ofSeq
-  ((x0, y0), (x1, y1))
-
-let enumSeg diags ((x0: int, y0: int), (x1, y1)) =
-  let xMin = Math.Min(x0, x1)
-  let xMax = Math.Max(x0, x1)
-  let yMin = Math.Min(y0, y1)
-  let yMax = Math.Max(y0, y1)
-  let count = Math.Max(xMax - xMin, yMax - yMin)
-  let enumPoints f = seq { 0 .. count } |> Seq.map f
-  if x0 = x1 then
-    enumPoints (fun i -> (x0, yMin + i))
-  elif y0 = y1 then
-    enumPoints (fun i -> (xMin + i, y0))
-  elif diags then
-    if x1 - x0 = y1 - y0 then
-      enumPoints (fun i -> (xMin + i, yMin + i))
-    else
-      enumPoints (fun i -> (xMin + i, yMax - i))
+  let lType =
+    if y0 = y1 then Horiz
+    elif x0 = x1 then Vert
+    elif y0-x0 = y1-x1 then ForwardDiag
+    else BackDiag
+  if x0 <= x1 then
+    (x0, y0), (x1, y1), lType
   else
-    Seq.empty
+    (x1, y1), (x0, y0), lType
 
-let countPoint map point =
-  map
-  |> Map.change point (fun n ->
-    Some (defaultArg n 0 |> (+) 1))
+let hasCollinear ((ax0, ay0), (ax1, ay1), at) x y =
+  assert ((ax0 = ax1 && ax0 = x) || (ay1-ay0)*(x-ax0) = (y-ay0)*(ax1-ax0))
+  let betweenInc a b c = a <= b && b <= c
+  match at with
+  | Vert -> betweenInc (mi ay0 ay1) y (ma ay0 ay1)
+  | _    -> betweenInc ax0 x ax1
+
+let intersectTypesOrdered a b =
+  let ((ax0, ay0), (ax1, ay1), at), ((bx0, by0), (bx1, by1), bt) = a, b
+  let considerPoint x y =
+    if hasCollinear a x y && hasCollinear b x y then
+      Seq.singleton (x, y)
+    else
+      Seq.empty
+  match at, bt with
+  | Horiz, Horiz when ay0 = by0 ->
+    { ma ax0 bx0 .. mi ax1 bx1 } |> Seq.map (fun x -> x, ay0)
+  | Vert, Vert when ax0 = bx0 ->
+    { ma (mi ay0 ay1) (mi by0 by1) .. mi (ma ay0 ay1) (ma by0 by1) } |> Seq.map (fun y -> ax0, y)
+  | Horiz, Vert -> considerPoint bx0 ay0
+  | ForwardDiag, ForwardDiag when ay0-ax0 = by0-bx0 ->
+    { ma ax0 bx0 .. mi ax1 bx1 } |> Seq.map (fun x -> x, x + (ay0-ax0))
+  | Horiz, ForwardDiag -> considerPoint (ay0 - (by0-bx0)) ay0
+  | Vert, ForwardDiag -> considerPoint ax0 (ax0 + (by0-bx0))
+  | BackDiag, BackDiag when ay0+ax0 = by0+bx0 ->
+    { ma ax0 bx0 .. mi ax1 bx1 } |> Seq.map (fun x -> x, (ay0+ax0) - x)
+  | Horiz, BackDiag -> considerPoint ((by0+bx0) - ay0) ay0
+  | Vert, BackDiag -> considerPoint ax0 ((by0+bx0) - ax0)
+  | ForwardDiag, BackDiag ->
+    let aIntercept = ay0-ax0
+    let doubX = (by0+bx0) - aIntercept
+    if doubX % 2 = 0 then
+      let x = doubX/2
+      considerPoint x (x + aIntercept)
+    else
+      Seq.empty
+  | _, _ -> Seq.empty
+
+let intersect l0 l1 =
+  let (_, _, t0), (_, _, t1) = l0, l1
+  if t0 <= t1 then intersectTypesOrdered l0 l1
+  else             intersectTypesOrdered l1 l0
+
+let rec intersectAll = function
+  | []      -> Seq.empty
+  | l :: ls -> Seq.append (ls |> Seq.collect (intersect l)) (intersectAll ls)
 
 let run diags =
   Seq.map parseLine
-  >> Seq.collect (enumSeg diags)
-  >> Seq.fold countPoint Map.empty
-  >> Map.values
-  >> Seq.filter ((<=) 2)
+  >> Seq.filter (fun (_, _, t) -> diags || t = Horiz || t = Vert)
+  >> List.ofSeq
+  >> intersectAll
+  >> Seq.distinct
   >> Seq.length
 
 let part1: (seq<string> -> int) = run false
