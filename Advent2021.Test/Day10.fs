17f15cc0a1d7f63b41dca214fbd7bb1c76feb14b
Kenneth Allen <kenneth@mostly.me>
2021-12-10 21:01:31 +1100

    Solve day 10

---
+++ Advent2021.Test/Day10.fs
@@ -0,0 +1,28 @@
+module Day10
+
+open Xunit
+open System.IO
+open Day10
+
+let in0 = [
+  "[({(<(())[]>[[{[]{<()<>>";
+  "[(()[<>])]({[<{<<[]>>(";
+  "{([(<{}[<>[]}>{[]{[(<()>";
+  "(((({<>}<{<{<>}{[]{[]{}";
+  "[[<[([]))<([[{}[[()]]]";
+  "[{[{({}]{}}([{[{{{}}([]";
+  "{<[[]]>}<{[{[{[]{()[[[]";
+  "[<(<(<(<{}))><([]([]()";
+  "<{([([[(<>()){}]>(<<{{";
+  "<{([{{}}[<[[[<>{}]]]>[]]"]
+let in1 = File.ReadLines "Input/10.txt" |> Seq.cache
+
+[<Fact>]
+let part1_in0 () = Assert.Equal(26397, part1 in0)
+[<Fact>]
+let part1_in1 () = Assert.Equal(369105, part1 in1)
+
+[<Fact>]
+let part2_in0 () = Assert.Equal(288957UL, part2 in0)
+[<Fact>]
+let part2_in1 () = Assert.Equal(3999363569UL, part2 in1)
---
+++ Advent2021/Day10.fs
@@ -0,0 +1,50 @@
+﻿module Day10
+
+let rec check stack i line =
+  let tryPop c =
+    match stack with
+    | head::tail when head = c -> check tail (i+1) line
+    | _                        -> Error c
+  let push c = check (c::stack) (i+1) line
+  match Seq.tryItem i line with
+  | Some '(' -> push '('
+  | Some '[' -> push '['
+  | Some '{' -> push '{'
+  | Some '<' -> push '<'
+  | Some ')' -> tryPop '('
+  | Some ']' -> tryPop '['
+  | Some '}' -> tryPop '{'
+  | Some '>' -> tryPop '<'
+  | None     -> Ok stack
+
+let part1 : string seq -> int =
+  Seq.map (check [] 0)
+  >> Seq.choose (
+    function
+    | Error '(' -> Some 3
+    | Error '[' -> Some 57
+    | Error '{' -> Some 1197
+    | Error '<' -> Some 25137
+    | Ok _      -> None)
+  >> Seq.sum
+
+let scoreIncomplete =
+  Seq.fold (fun score c ->
+    score*5UL + 
+      match c with
+      | '(' -> 1UL
+      | '[' -> 2UL
+      | '{' -> 3UL
+      | '<' -> 4UL) 0UL
+
+let middle ns = Seq.item (Seq.length ns / 2) ns
+
+let part2 : string seq -> uint64 =
+  Seq.map (check [] 0)
+  >> Seq.choose (
+    function
+    | Ok stack when not (List.isEmpty stack) ->
+      Some (scoreIncomplete stack)
+    | Error _ -> None)
+  >> Seq.sort
+  >> middle
