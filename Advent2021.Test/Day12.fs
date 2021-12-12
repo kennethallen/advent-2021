b98ec5bd38bb0660d96df899e150d1ce75c48312
Kenneth Allen <kenneth@mostly.me>
2021-12-12 20:56:59 +1100

    Solve day 12

---
+++ Advent2021.Test/Day12.fs
@@ -0,0 +1,56 @@
+module Day12
+
+open Xunit
+open System.IO
+open Day12
+
+let in0 = ["start-A";"start-b";"A-c";"A-b";"b-d";"A-end";"b-end"]
+let in1 = [
+  "dc-end";
+  "HN-start";
+  "start-kj";
+  "dc-start";
+  "dc-HN";
+  "LN-dc";
+  "HN-end";
+  "kj-sa";
+  "kj-HN";
+  "kj-dc"]
+let in2 = [
+  "fs-end";
+  "he-DX";
+  "fs-he";
+  "start-DX";
+  "pj-DX";
+  "end-zg";
+  "zg-sl";
+  "zg-pj";
+  "pj-he";
+  "RW-he";
+  "fs-DX";
+  "pj-RW";
+  "zg-RW";
+  "start-pj";
+  "he-WI";
+  "zg-he";
+  "pj-fs";
+  "start-RW"]
+let in3 = File.ReadLines "Input/12.txt" |> Seq.cache
+
+[<Fact>]
+let part1_in0 () = Assert.Equal(10, part1 in0)
+[<Fact>]
+let part1_in1 () = Assert.Equal(19, part1 in1)
+[<Fact>]
+let part1_in2 () = Assert.Equal(226, part1 in2)
+[<Fact>]
+let part1_in3 () = Assert.Equal(4495, part1 in3)
+
+[<Fact>]
+let part2_in0 () = Assert.Equal(36, part2 in0)
+[<Fact>]
+let part2_in1 () = Assert.Equal(103, part2 in1)
+[<Fact>]
+let part2_in2 () = Assert.Equal(3509, part2 in2)
+[<Fact>]
+let part2_in3 () = Assert.Equal(131254, part2 in3)
---
+++ Advent2021/Day12.fs
@@ -0,0 +1,40 @@
+﻿module Day12
+#nowarn "25"
+
+open System
+
+let isLarge (c : string) =
+  Char.IsUpper c[0]
+
+let withDiEdge c0 c1 =
+  Map.change c0 (fun s -> defaultArg s Set.empty |> Set.add c1 |> Some)
+
+let withEdge c0 c1 =
+  withDiEdge c0 c1 >> withDiEdge c1 c0
+    
+let build : string seq -> Map<string, Set<string>> =
+  Seq.map (fun s ->
+    let [|s0; s1|] = s.Split("-")
+    s0, s1)
+  >> Seq.fold (fun m (c0, c1) -> withEdge c0 c1 m) Map.empty
+
+let countPaths idb m =
+  let rec f sv db n =
+    if n = "end" then 1
+    else
+      let sv' =
+        if isLarge n then sv
+        else              Set.add n sv
+      let dys, dns =
+        Map.find n m
+        |> Set.partition (fun d -> Set.contains d sv)
+      let ddbs =
+        if db then
+          dys |> Seq.except ["start"] |> Seq.map (f sv' false)
+        else Seq.empty
+      Seq.append ddbs (dns |> Seq.map (f sv' db))
+      |> Seq.sum
+  f Set.empty idb "start"
+
+let part1 : string seq -> int = build >> countPaths false
+let part2 : string seq -> int = build >> countPaths true
