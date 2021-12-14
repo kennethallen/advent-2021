50d46ea6456e432e7cc219724fd67c78f24a5de1
Kenneth Allen <kenneth@mostly.me>
2021-12-14 23:31:27 +1100

    Solve day 14 part 1

---
+++ Advent2021.Test/Day14.fs
@@ -0,0 +1,36 @@
+module Day14
+
+open Xunit
+open System.IO
+open Day14
+
+let in0 = [
+  "NNCB";
+  "";
+  "CH -> B";
+  "HH -> N";
+  "CB -> H";
+  "NH -> C";
+  "HB -> C";
+  "HC -> B";
+  "HN -> C";
+  "NN -> C";
+  "BH -> H";
+  "NC -> B";
+  "NB -> B";
+  "BN -> B";
+  "BB -> N";
+  "BC -> B";
+  "CC -> N";
+  "CN -> C"]
+let in1 = File.ReadLines "Input/14.txt" |> Seq.cache
+
+[<Fact>]
+let part1_in0 () = Assert.Equal(1588UL, part1 in0)
+[<Fact>]
+let part1_in1 () = Assert.Equal(2937UL, part1 in1)
+
+[<Fact>]
+let part2_in0 () = Assert.Equal(2188189693529UL, part2 in0)
+[<Fact>]
+let part2_in1 () = Assert.Equal(2937UL, part2 in1)
---
+++ Advent2021/Day14.fs
@@ -0,0 +1,41 @@
+﻿module Day14
+#nowarn "25"
+
+open System.Text.RegularExpressions
+
+let regex = Regex "^(.)(.) -> (.)$"
+let parse ls =
+  let temp = Seq.head ls |> List.ofSeq
+  let rules =
+    ls
+    |> Seq.skip 2
+    |> Seq.map (fun s ->
+      let [a; b; c] =
+        (regex.Match s).Groups
+        |> Seq.tail
+        |> Seq.map (fun g -> g.Value[0])
+        |> List.ofSeq
+      (a, b), c)
+    |> Map.ofSeq
+  temp, rules
+
+let rec step rules es =
+  es
+  |> Seq.pairwise
+  |> Seq.collect (fun (e0, e1) ->
+    [Map.find (e0, e1) rules; e1])
+  |> Seq.append (Seq.truncate 1 es)
+
+let run n ls =
+  let temp, rules = parse ls
+  let counts =
+    Seq.unfold (step rules >> (fun x -> Some (x, x))) temp
+    |> Seq.item (n-1)
+    |> Seq.fold (fun m e ->
+      Map.change e (Option.map ((+) 1UL) >> Option.orElse (Some 1UL)) m)
+      Map.empty
+    |> Map.values
+  (Seq.max counts) - (Seq.min counts)
+
+let part1 : string seq -> uint64 = run 10
+let part2 : string seq -> uint64 = run 40
