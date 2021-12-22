f0847ef69d84ebab2ac77edf7a441eff56753503
Kenneth Allen <kenneth@mostly.me>
2021-12-22 19:16:08 +1100

    Attempt octree solution for day 22 part 1

--- Advent2021/Day22.fs
+++ Advent2021/Day22.fs
@@ -3,81 +3,70 @@
 open System
 open System.Text.RegularExpressions
 
-type RPrism = {
-  x0: int64
-  x1: int64
-  y0: int64
-  y1: int64
-  z0: int64
-  z1: int64
+type Node<'a> =
+  | State of bool
+  | Children of 'a list
+
+type Octree = {
+  X: int64
+  Y: int64
+  Z: int64
+  Dim: int64
+  Node: Node<Octree>
 }
 
-module RPrism =
-  let empty p =
-    p.x0 > p.x1 || p.y0 > p.y1 || p.z0 > p.z1
+type RPrism = {
+  X0: int64
+  X1: int64
+  Y0: int64
+  Y1: int64
+  Z0: int64
+  Z1: int64
+}
 
-  let tryMergeNeighbors p0 p1 =
-    if p0.x0 = p1.x0 && p0.x1 = p1.x1 then
-      if p0.y0 = p1.y0 && p0.y1 = p1.y1 then
-        if p1.z0 = p0.z1+1L then
-          Some {p0 with z1=p1.z1}
-        elif p0.z0 = p1.z1+1L then
-          Some {p1 with z1=p0.z1}
-        else
-          None
-      elif p0.z0 = p1.z0 && p0.z1 = p1.z1 then
-        if p1.y0 = p0.y1+1L then
-          Some {p0 with y1=p1.y1}
-        elif p0.y0 = p1.y1+1L then
-          Some {p1 with y1=p0.y1}
-        else
-          None
-      else None
-    elif p0.y0 = p1.y0 && p0.y1 = p1.y1 && p0.z0 = p1.z0 && p0.z1 = p1.z1 then
-      if p1.x0 = p0.x1+1L then
-        Some {p0 with x1=p1.x1}
-      elif p0.x0 = p1.x1+1L then
-        Some {p1 with x1=p0.x1}
-      else
-        None
-    else None
-
-  let consolidate ps =
-    let rec f ps =
-      function
-      | [] -> ps
-      | next::rest ->
-        match List.tryPick (fun p -> tryMergeNeighbors next p |> Option.map (fun c -> c, p)) ps with
-        | None -> f (next::ps) rest
-        | Some (c, p) -> f (List.except [p] ps) (c::ps)
-    f [] ps
-
-  let carveOut m p =
-    if m.x0 > p.x1 && p.x0 > m.x1 && m.y0 > p.y1 && p.y0 > m.y1 && m.z0 > p.z1 && p.z0 > m.z1 then
-      [p]
-    else
-      seq {p.z0,m.z0-1L; m.z0,m.z1; m.z1+1L,p.z1}
-      |> Seq.allPairs (seq {p.y0,m.y0-1L; m.y0,m.y1; m.y1+1L,p.y1})
-      |> Seq.allPairs (seq {p.x0,m.x0-1L; m.x0,m.x1; m.x1+1L,p.x1})
-      |> Seq.removeAt 4 // Center section, hole left by mask
-      |> Seq.map (fun ((x0, x1), ((y0, y1), (z0, z1))) -> {x0=x0; x1=x1; y0=y0; y1=y1; z0=z0; z1=z1})
-      |> Seq.filter (empty >> not)
+module Octree =
+  let makeChildren o =
+    match o.Node with
+    | Children cs -> cs
+    | State s ->
+      let halfDim = o.Dim >>> 1
+      [o.Z; o.Z + halfDim]
+      |> Seq.allPairs [o.Y; o.Y + halfDim]
+      |> Seq.allPairs [o.X; o.X + halfDim]
+      |> Seq.map (fun (x, (y, z)) -> {X=x; Y=y; Z=z; Dim=halfDim; Node=State s})
       |> List.ofSeq
-      |> consolidate
-      
-  let clip m p =
-    let p = {
-      x0 = Math.Max(p.x0, m.x0)
-      x1 = Math.Min(p.x1, m.x1)
-      y0 = Math.Max(p.y0, m.y0)
-      y1 = Math.Min(p.y1, m.y1)
-      z0 = Math.Max(p.z0, m.z0)
-      z1 = Math.Min(p.z1, m.z1)
-    }
-    if empty p then None else Some p
 
-  let card p =
-    (p.x1+1L-p.x0) * (p.y1+1L-p.y0) * (p.z1+1L-p.z0)
+  let withinPrism p (o: Octree) =
+    p.X0 <= o.X && o.X+o.Dim <= p.X1
+    && p.Y0 <= o.Y && o.Y+o.Dim <= p.Y1
+    && p.Z0 <= o.Z && o.Z+o.Dim <= p.Z1
+
+  let disjointFromPrism p (o: Octree) =
+    p.X1 < o.X || o.X+o.Dim < p.X0
+    || p.Y1 < o.Y || o.Y+o.Dim < p.Y0
+    || p.Z1 < o.Z || o.Z+o.Dim < p.Z0
+
+  let rec apply s p o =
+    if disjointFromPrism p o then
+      o
+    elif withinPrism p o then
+      {o with Node=State s}
+    else
+      {o with Node=Children (makeChildren o |> List.map (apply s p))}
+
+  let rec clip p o =
+    if disjointFromPrism p o then
+      {o with Node=State false}
+    elif withinPrism p o then
+      o
+    else
+      {o with Node=Children (makeChildren o |> List.map (clip p))}
+
+  let rec card o =
+    match o.Node with
+    | State true -> o.Dim * o.Dim * o.Dim
+    | State false -> 0L
+    | Children cs -> cs |> List.sumBy card
 
 let regex = Regex @"^(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)$"
 let parse: string seq -> (bool * RPrism) seq =
@@ -88,16 +77,31 @@ let parse: string seq -> (bool * RPrism) seq =
       |> Seq.map (fun g -> g.Value)
       |> List.ofSeq
     let [x0; x1; y0; y1; z0; z1] = coords |> List.map int64
-    state = "on", {x0=x0; x1=x1; y0=y0; y1=y1; z0=z0; z1=z1})
+    state = "on", {X0=x0; X1=x1; Y0=y0; Y1=y1; Z0=z0; Z1=z1})
 
-let step zones (state, newPrism) =
-  let zones = zones |> List.collect (RPrism.carveOut newPrism)
-  if state then newPrism::zones else zones
+let step octree (state, prism) = Octree.apply state prism octree
+
+let po2Ceil n =
+  let rec f p = if p >= n then p else f (p <<< 1)
+  f 1L
   
-let part1: string seq -> int64 =
-  let bounds = {x0= -50L; x1=50L; y0= -50L; y1=50L; z0= -50L; z1=50L}
-  parse
-  >> Seq.fold step []
-  >> Seq.choose (RPrism.clip bounds)
-  >> Seq.map RPrism.card
-  >> Seq.sum
+let part1 ls =
+  let bounds = {X0= -50L; X1=50L; Y0= -50L; Y1=50L; Z0= -50L; Z1=50L}
+  let zones = parse ls
+  let maxCoord =
+    zones
+    |> Seq.collect (fun (_, p) -> [p.X0; p.X1; p.Y0; p.Y1; p.Z0; p.X1])
+    |> Seq.map Math.Abs
+    |> Seq.max
+    |> (+) 1L
+    |> po2Ceil
+  let octree = {
+    X = -maxCoord
+    Y = -maxCoord
+    Z = -maxCoord
+    Dim = 2L*maxCoord
+    Node = State false}
+  zones
+  |> Seq.fold step octree
+  |> Octree.clip bounds
+  |> Octree.card
